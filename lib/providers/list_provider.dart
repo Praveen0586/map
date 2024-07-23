20ccf49726e0b16576f4fe9e2c5b6146885e12e0
Praveen0586 <praveensuresh1605@gmail.com>
2024-07-23 20:38:31 +0530

    storing image as in oacl Directories - by using Path_provider and Path packages - working

--- lib/providers/list_provider.dart
+++ lib/providers/list_provider.dart
@@ -2,12 +2,17 @@ import 'dart:io';
 
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:map/data/locationdatas.dart';
+import 'package:path_provider/path_provider.dart' as syspath;
+import 'package:path/path.dart' as path;
 
 class AddStateNotifier extends StateNotifier<List<Locationdatas>> {
   AddStateNotifier() : super(const []);
 
-  void additem(String title, File image, LocationDetails location) {
-    var newitem = Locationdatas(title, image, location);
+  void additem(String title, File image, LocationDetails location) async {
+    final filename = path.basename(image.path);
+    final appDir = await syspath.getApplicationDocumentsDirectory();
+    final copiedpath = await image.copy('${appDir.path}/$filename');
+    var newitem = Locationdatas(title, copiedpath, location);
     state = [newitem, ...state];
   }
 }
