97e115b4498cf9612b264a6a8e8a2157885d6b02
Praveen0586 <praveensuresh1605@gmail.com>
2024-07-23 22:35:02 +0530

    storing datas in sql data base

--- lib/providers/list_provider.dart
+++ lib/providers/list_provider.dart
@@ -4,6 +4,8 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:map/data/locationdatas.dart';
 import 'package:path_provider/path_provider.dart' as syspath;
 import 'package:path/path.dart' as path;
+import 'package:sqflite/sqflite.dart' as sql;
+import 'package:sqflite/sqlite_api.dart';
 
 class AddStateNotifier extends StateNotifier<List<Locationdatas>> {
   AddStateNotifier() : super(const []);
@@ -14,6 +16,23 @@ class AddStateNotifier extends StateNotifier<List<Locationdatas>> {
     final copiedpath = await image.copy('${appDir.path}/$filename');
     var newitem = Locationdatas(title, copiedpath, location);
     state = [newitem, ...state];
+
+    final dbpath = await sql.getDatabasesPath();
+    final database = await sql.openDatabase(
+      path.join(dbpath, 'user_files.db'),
+      onCreate: (db, version) {
+        return db.execute(
+            'CREATE TABLE user_place ( id TEXT PRIMARY KEY,title TEXT,lat REAL,long REAL,image TEXT )');
+      },
+      version: 1,
+    );
+    database.insert('user_place', {
+      'id': newitem.id,
+      'title': newitem.title,
+      'lat': newitem.place.latitude,
+      'long': newitem.place.longitude,
+      'image': newitem.imgae.path
+    });
   }
 }
 
